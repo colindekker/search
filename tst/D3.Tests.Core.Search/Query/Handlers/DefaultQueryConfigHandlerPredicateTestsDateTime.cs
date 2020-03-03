77a6485924beac83757f5f53db31a58a0bb0c0c2
Colin Dekker <colin.dekker@outlook.com>
2020-03-03 21:54:57 +0100

    test(predicate tests): add more test cases

--- tst/D3.Tests.Core.Search/Query/Handlers/DefaultQueryConfigHandlerPredicateTestsDateTime.cs
+++ tst/D3.Tests.Core.Search/Query/Handlers/DefaultQueryConfigHandlerPredicateTestsDateTime.cs
@@ -22,30 +22,124 @@ namespace D3.Tests.Core.Search.Query.Handlers
         private readonly ServiceProvider _serviceProvider;
 
         [Fact]
-        public async Task Handler_Finds_Single_Item_By_Single_Predicate_DateTime_Property_Between()
+        public async Task Handler_Finds_Multiple_Items_By_Single_Predicate_For_DateTime_Property_With_Multiple_Or_Values_Incorrect_Between()
         {
             var handler = _serviceProvider
                 .GetService<IQueryConfigHandler<QueryConfig>>();
 
-            var config = new QueryConfig { QueryBy = new List<QueryPredicate>() };
-
-            var p1 = new QueryPredicate
+            var config = new QueryConfig
             {
-                ColumnCode = "Start",
-                CompareWith = QueryPredicateConnective.And,
-                Values = new List<QueryPredicateValue>()
+                QueryBy = new List<QueryPredicate>
+                {
+                    new QueryPredicate
+                    {
+                        ColumnCode = "Start",
+                        CompareWith = QueryPredicateConnective.Or,
+                        Values = new List<QueryPredicateValue>
+                        {
+                            new QueryPredicateValue
+                            {
+                                Value = "2020-01-14",
+                                CompareUsing = QueryPredicateComparison.GreaterThan,
+                                CompareWith = QueryPredicateConnective.Or,
+                                Sequence = 1
+                            },
+                            new QueryPredicateValue
+                            {
+                                Value = "2020-01-16",
+                                CompareUsing = QueryPredicateComparison.LessThanOrEqual,
+                                CompareWith = QueryPredicateConnective.Or,
+                                Sequence = 2
+                            }
+                        }
+                    }
+                }
             };
-
-            p1.Values.Add(new QueryPredicateValue { Value = "2020-01-14", CompareUsing = QueryPredicateComparison.GreaterThan });
-            p1.Values.Add(new QueryPredicateValue { Value = "2020-01-16", CompareUsing = QueryPredicateComparison.LessThanOrEqual });
-
-            config.QueryBy.Add(p1);
 
             var source = new List<TestItem>
             {
-                new TestItem { Name = "Colin", Id = 1, Start = new DateTime(2020, 1, 1)},
-                new TestItem { Name = "Brendan", Id = 2, Start = new DateTime(2020, 2, 1)},
-                new TestItem { Name = "Lindsay", Id = 3, Start = new DateTime(2020, 1, 15)}
+                new TestItem
+                {
+                    Name = "Colin",
+                    Id = 1,
+                    Start = new DateTime(2020, 1, 1)
+                },
+                new TestItem
+                {
+                    Name = "Brendan",
+                    Id = 2,
+                    Start = new DateTime(2020, 2, 1)
+                },
+                new TestItem
+                {
+                    Name = "Lindsay",
+                    Id = 3,
+                    Start = new DateTime(2020, 1, 15)
+                }
+            };
+
+            var result = await handler
+                .HandleAsync<TestItem, QueryResult<TestItem>>(config, source.AsQueryable())
+                .ConfigureAwait(false);
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Total);
+            Assert.NotEmpty(result.Payload);
+        }
+
+        [Fact]
+        public async Task Handler_Finds_Single_Item_By_Single_Predicate_For_DateTime_Property_With_Multiple_And_Values_Between()
+        {
+            var handler = _serviceProvider
+                .GetService<IQueryConfigHandler<QueryConfig>>();
+
+            var config = new QueryConfig
+            {
+                QueryBy = new List<QueryPredicate>
+                {
+                    new QueryPredicate
+                    {
+                        ColumnCode = "Start",
+                        CompareWith = QueryPredicateConnective.And,
+                        Values = new List<QueryPredicateValue>
+                        {
+                            new QueryPredicateValue
+                            {
+                                Value = "2020-01-14",
+                                CompareUsing = QueryPredicateComparison.GreaterThan,
+                                Sequence = 1
+                            },
+                            new QueryPredicateValue
+                            {
+                                Value = "2020-01-16",
+                                CompareUsing = QueryPredicateComparison.LessThanOrEqual,
+                                Sequence = 2
+                            }
+                        }
+                    }
+                }
+            };
+
+            var source = new List<TestItem>
+            {
+                new TestItem
+                {
+                    Name = "Colin",
+                    Id = 1,
+                    Start = new DateTime(2020, 1, 1)
+                },
+                new TestItem 
+                {
+                    Name = "Brendan",
+                    Id = 2,
+                    Start = new DateTime(2020, 2, 1)
+                },
+                new TestItem
+                {
+                    Name = "Lindsay",
+                    Id = 3,
+                    Start = new DateTime(2020, 1, 15)
+                }
             };
 
             var result = await handler
